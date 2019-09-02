5d8a2949654bbfa5fb089da7d4588ab2c5b78ff6
rajk8888 <54786066+rajk8888@users.noreply.github.com>
2019-09-02 12:22:43 +0100

    Adding in another test to get up code coverage score. Tests where no name param has bveen supplied.

--- src/test/java/com/functionp/FunctionTest.java
+++ src/test/java/com/functionp/FunctionTest.java
@@ -92,4 +92,40 @@ public class FunctionTest {
         assertEquals(ret.getBody(),"A Very Special Hello To, World");
     }
     
+    /**
+     * Unit test for HttpTriggerJava method.
+     */
+    @Test
+    public void testHttpTriggerJavaBadRequest() throws Exception {
+        // Setup
+        @SuppressWarnings("unchecked")
+        final HttpRequestMessage<Optional<String>> req = mock(HttpRequestMessage.class);
+
+        final Map<String, String> queryParams = new HashMap<>();
+        
+        //pass in anything but NOT the name
+        queryParams.put("otherstuff", "blahblah");
+        
+        doReturn(queryParams).when(req).getQueryParameters();
+
+        final Optional<String> queryBody = Optional.empty();
+        doReturn(queryBody).when(req).getBody();
+
+        doAnswer(new Answer<HttpResponseMessage.Builder>() {
+            @Override
+            public HttpResponseMessage.Builder answer(InvocationOnMock invocation) {
+                HttpStatus status = (HttpStatus) invocation.getArguments()[0];
+                return new HttpResponseMessageMock.HttpResponseMessageBuilderMock().status(status);
+            }
+        }).when(req).createResponseBuilder(any(HttpStatus.class));
+
+        final ExecutionContext context = mock(ExecutionContext.class);
+        doReturn(Logger.getGlobal()).when(context).getLogger();
+
+        // Invoke
+        final HttpResponseMessage ret = new Function().run(req, context);
+
+        // Verify
+        assertEquals(ret.getStatus(), HttpStatus.BAD_REQUEST);
+    }
 }
